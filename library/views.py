3aeada29e0ef6ef115b947186c6c9b3c3164975b
Jeremy Boyd <jeremy.boyd@mapmyfitness.com>
2012-04-25 20:58:24 -0500

    added book_index view and urlconf

--- library/views.py
+++ library/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from library.models import Book
 
 def hello_world(request):
     return HttpResponse('Hi, AWPUG!')
+
+def book_index(request):
+    books = Book.objects.all()
+
+    output = '<h1>Every book we know about:</h1>'
+    output += '<ul>'
+    for book in books:
+        output += '<li>' + book.title + '</li>'
+    output += '</ul>'
+
+    return HttpResponse(output)
--- urls.py
+++ urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^hello_world/$', 'library.views.hello_world'),
+    url(r'^books/$', 'library.views.book_index'),
 )
